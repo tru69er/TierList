70102ee905eee62863710738ade8c3a707e769b6
tru69er <chidori9912@gmail.com>
2023-09-08 00:15:01 +0530

    remove rust binary

--- main.rs
+++ main.rs
@@ -3,14 +3,18 @@ use std::fs::read_dir;
 fn main() {
     let paths = read_dir("./src/lib/images").unwrap();
 
-    let mut i = 0;
+    let mut i = 0, j = 0;
     for path in paths {
-        i += 1;
         println!(
-            "{{\n\tid: {},\n\tpath: \"{}\",\n}},",
+            "{{\n\tpos: {} {},\n\tpath: \"{}\",\n}},",
             i,
             path.unwrap().path().display()
         );
+        j += 1;
+        if j == 7 {
+            i += 1;
+            j = 0;
+        }
     }
 }
 
