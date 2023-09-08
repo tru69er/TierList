6871876b50c5611015da811e171e89d0c8a3c659
tru69er <chidori9912@gmail.com>
2023-09-08 19:39:58 +0530

    done ig lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,134 +1,197 @@
 <script lang="ts">
-  const tiers = [
-    { name: "Jod", color: "Red", albums: [] },
+  type Album = {
+    pos: number;
+    path: string;
+  };
+
+  type Tier = {
+    name: string;
+    color: string;
+    albums: Album[];
+  };
+
+  const tiers: Tier[] = [
+    { name: "S", color: "Red", albums: [] },
     { name: "A", color: "Orange", albums: [] },
     { name: "B", color: "Yellow", albums: [] },
     { name: "C", color: "Green", albums: [] },
-    { name: "Dogshit", color: "Blue", albums: [] },
+    { name: "D", color: "Blue", albums: [] },
   ];
 
-  const albums = [
+  const tierAlbums: Album[][] = [[], [], [], [], []];
+
+  let selAlbum: Album | undefined;
+
+  const albums: Album[] = [
     {
-      pos: [0, 0],
+      pos: 0,
       path: "./src/lib/images/amnesiac.png",
     },
     {
-      pos: [0, 1],
+      pos: 1,
       path: "./src/lib/images/angles.jpg",
     },
     {
-      pos: [0, 2],
+      pos: 2,
       path: "./src/lib/images/bends.png",
     },
     {
-      pos: [0, 3],
+      pos: 3,
       path: "./src/lib/images/comedownmachine.jpg",
     },
     {
-      pos: [0, 4],
+      pos: 4,
       path: "./src/lib/images/firstimpressionsofearth.jpg",
     },
     {
-      pos: [0, 5],
+      pos: 5,
       path: "./src/lib/images/hailtothethief.png",
     },
     {
-      pos: [0, 6],
+      pos: 6,
       path: "./src/lib/images/inrainbows.png",
     },
     {
-      pos: [1, 0],
+      pos: 7,
       path: "./src/lib/images/isthisit.png",
     },
     {
-      pos: [1, 1],
+      pos: 8,
       path: "./src/lib/images/kida.png",
     },
     {
-      pos: [1, 2],
+      pos: 9,
       path: "./src/lib/images/kingoflimbs.png",
     },
     {
-      pos: [1, 3],
+      pos: 10,
       path: "./src/lib/images/moonshapedpool.png",
     },
     {
-      pos: [1, 4],
+      pos: 11,
       path: "./src/lib/images/newabnormal.jpg",
     },
     {
-      pos: [1, 5],
+      pos: 12,
       path: "./src/lib/images/okcomputer.png",
     },
     {
-      pos: [1, 6],
+      pos: 13,
       path: "./src/lib/images/pablohoney.png",
     },
     {
-      pos: [2, 0],
+      pos: 14,
       path: "./src/lib/images/roomonfire.png",
     },
   ];
 
   let selState = 0;
-
-  let selIndex = [0, 0];
+  let selIndex = 0;
+  let selTier = 0;
 
   function move(e: KeyboardEvent) {
-    switch (e.key) {
-      case "h":
-        if (selIndex[1] > 0) {
-          selIndex[1]--;
-        }
-        break;
-      case "j":
-        if (selIndex[0] < 2) {
-          selIndex[0]++;
-        }
-        if (selIndex[0] === 2) {
-          selIndex[1] = 0;
-        }
-        break;
-      case "k":
-        if (selIndex[0] > 0) {
-          selIndex[0]--;
-        }
-        break;
-      case "l":
-        if (selIndex[0] !== 2 && selIndex[1] < 6) {
-          selIndex[1]++;
-        }
-        break;
-      default:
-        break;
+    if (selState === 1) {
+      switch (e.key) {
+        case "k":
+          if (selTier > 0) {
+            tierAlbums[selTier] = [
+              ...tierAlbums[selTier].slice(0, tierAlbums[selTier--].length - 1),
+            ];
+            if (selAlbum) {
+              tierAlbums[selTier] = [...tierAlbums[selTier], selAlbum];
+            }
+          }
+          break;
+        case "j":
+          if (selTier < 4) {
+            tierAlbums[selTier] = [
+              ...tierAlbums[selTier].slice(0, tierAlbums[selTier++].length - 1),
+            ];
+            if (selAlbum) {
+              tierAlbums[selTier] = [...tierAlbums[selTier], selAlbum];
+            }
+          }
+          break;
+        case "Enter":
+          if (albums.length === 0) {
+            selState = 2;
+            break;
+          } else {
+            selState = 0;
+            selIndex = 0;
+            selTier = 0;
+          }
+          break;
+        default:
+          break;
+      }
+    } else if (selState === 0) {
+      switch (e.key) {
+        case "Enter":
+          selState = 1;
+
+          selAlbum = albums[selIndex];
+          if (selAlbum) {
+            albums.splice(albums.indexOf(selAlbum), 1);
+            tierAlbums[0] = [...tierAlbums[0], selAlbum];
+          }
+
+          selIndex = 50;
+          break;
+        case "h":
+          if (selIndex > 0) {
+            selIndex--;
+          }
+          break;
+        case "j":
+          if (selIndex + 7 <= albums.length - 1) selIndex += 7;
+          break;
+        case "k":
+          if (selIndex - 7 >= 0) {
+            selIndex -= 7;
+          }
+          break;
+        case "l":
+          if (selIndex + 1 < albums.length) {
+            selIndex++;
+          }
+          break;
+        default:
+          break;
+      }
+    } else {
     }
   }
 
-  function match(index: number[], _: number[]): boolean {
-    return index[0] === selIndex[0] && index[1] === selIndex[1];
+  function match(album: Album, _: number): boolean {
+    return albums.indexOf(album) === selIndex;
   }
 </script>
 
 <div class="page">
   <div class="container">
-    <div class="title">Radiohead and Strokes Albums Tier List</div>
+    <div class="title">Radiohead and Strokes Album Tier List</div>
     <div class="tiers">
-      {#each tiers as tier (tier)}
+      {#each tiers as tier, index (tier.name)}
         <div class="row">
           <div class="tier {tier.color} ">
             <span class="tiername">{tier.name}</span>
           </div>
-          <div class="albums" />
+          <div class="tierAlbums">
+            {#each tierAlbums[index] as album (album.path)}
+              <img src={album.path} alt="" class="tierAlbum" />
+            {/each}
+          </div>
         </div>
       {/each}
     </div>
   </div>
   <div class="albums {selState === 0 ? 'pickAlbum' : ''}">
-    {#each albums as album (album)}
+    {#each albums as album (album.path)}
       <img
         src={album.path}
         alt=""
-        class={match(album.pos, selIndex) ? "selAlbum" : ""}
+        class="albumsImg {match(album, selIndex) ? 'selAlbum' : ''}"
       />
     {/each}
   </div>
@@ -186,17 +249,32 @@
     font-size: x-large;
   }
 
+  .tierAlbums {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 100%;
+  }
+
+  .tierAlbum {
+    height: 93%;
+    width: 10%;
+    padding-left: 1%;
+  }
+
   .albums {
     margin: 2.5% 0;
     display: grid;
-    grid-template-columns: repeat(7, 14.7%);
+    grid-template-columns: repeat(7, 14.6%);
     width: 68%;
     padding: 1%;
+    background-color: rgb(41, 40, 40);
   }
 
-  img {
+  .albumsImg {
     width: 80%;
     height: 80%;
+    border: 4px solid rgb(41, 40, 40);
   }
 
   .pickAlbum {
